57b45e25e445ce208a0f791d65d56f7c24f9af6c
currychoco <godqhrgka0000@naver.com>
2023-02-08 16:56:11 +0900

    어노테이션 부분 주석 추가

--- src/main/java/site/currychoco/hrmanager/core/annotation/CheckAuthority.java
+++ src/main/java/site/currychoco/hrmanager/core/annotation/CheckAuthority.java
@@ -2,10 +2,10 @@ package site.currychoco.hrmanager.core.annotation;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@Target(ElementType.METHOD) //어노테이션을 작성할 곳
+@Retention(RetentionPolicy.RUNTIME) // 어노테이션의 지속 시간 설정
+@Documented // Java doc에 문서화 여부 결정
 public @interface CheckAuthority {
 
-    String authCode();
+    String authCode(); // 파라미터 멤버
 }
